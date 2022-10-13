2d829a8f92d071e8a840cec39cb7e5cb11ab5cce
rayan <rayan@debian>
2022-10-13 12:13:05 +0200

    modification du job 8

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -1,3 +1,3 @@
 date=$(date +%d-%m-%Y-%H:%M) 
 
-last -f /var/log/wtmp rayan > nombre_de_logs.txt; touch ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && wc -l nombre_de_logs.txt > ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && rm nombre_de_logs.txt && tar -c -f ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.tar.gz ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt
+last -f /var/log/auth.log > nombre_de_logs.txt; touch ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && wc -l nombre_de_logs.txt > ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && rm nombre_de_logs.txt && tar -c -f ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.tar.gz ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt
