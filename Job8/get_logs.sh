1240d10b4e884abd1c554352cf246243fedfd7a0
rayan <rayan@debian>
2022-10-13 12:58:14 +0200

    correction de get_logs.sh qui agit sur le bon fichier auth.log

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -1,3 +1,3 @@
 date=$(date +%d-%m-%Y-%H:%M) 
 
-last -f /var/log/auth.log > nombre_de_logs.txt; touch ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && wc -l nombre_de_logs.txt > ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && rm nombre_de_logs.txt && tar -c -f ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.tar.gz ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt
+sudo tail  /var/log/auth.log | sudo grep -i "opened for user rayan" /var/log/auth.log > nombre_de_logs.txt; touch ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && wc -l nombre_de_logs.txt > ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt && rm nombre_de_logs.txt && tar -c -f ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.tar.gz ~/Documents/git/shell.exe-/Job8/Backup/number_connection-$date.txt
